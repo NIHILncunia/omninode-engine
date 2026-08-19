e946bc7f9129085c84fcd9109531b3402e3926dc
NIHILncunia <nihil_ncunia@naver.com>
2026-08-19 23:33:04 +0900

    2026 0819 refactor: 카테고리 스키마 정합화

--- server/db/table/categories.table.ts
+++ server/db/table/categories.table.ts
@@ -1,18 +1,22 @@
-﻿import { bigint, foreignKey, pgTable, smallint, varchar } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import { check, foreignKey, pgTable, smallint, varchar } from 'drizzle-orm/pg-core';
 import { admins } from './admins.table';
 import { commonColumns } from './common.columns';
-import { worlds } from './worlds.table';
 
 export const categories = pgTable('categories', {
   ...commonColumns(() => admins.id),
-  worldId: bigint('world_id', { mode: 'number', })
-    .references(() => worlds.id, { onDelete: 'no action', }),
-  parentId: bigint('parent_id', { mode: 'number', }),
+  parentId: smallint('parent_id', { mode: 'number', }),
   name: varchar('name')
     .notNull(),
   depth: smallint('depth')
     .notNull(),
-  defaultYn: varchar('default_yn', { length: 1, })
+  defaultYn: varchar('default_yn', {
+    enum: [
+      'Y',
+      'N',
+    ],
+    length: 1,
+  })
     .notNull()
     .default('N'),
 }, table => [
@@ -26,4 +30,5 @@ export const categories = pgTable('categories', {
     name: 'fk_categories_parent_id',
   })
     .onDelete('no action'),
+  check('ck_categories_depth', sql`${table.depth} between 1 and 3`),
 ]);
